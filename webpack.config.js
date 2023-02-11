63791c4f97d19a53d23cb6e1e9809944f2d6d3e5
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-02-11 10:23:47 -0800

    add preact i guess

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: {
+    app: "./client/index.js",
+  },
+
+  output: {
+    path: path.join(__dirname, "dist"),
+
+    filename: "[name].js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
